d88d785a1a8afd6210e7119188eeb04c8ae77d08
Dawnbells.W <838506906@qq.com>
2026-08-25 01:20:16 -0700

    skuNames

--- v7-shop-services/v7-shop-admin/src/main/java/cn/v7soft/admin/service/dto/OrderDownloadDto.java
+++ v7-shop-services/v7-shop-admin/src/main/java/cn/v7soft/admin/service/dto/OrderDownloadDto.java
@@ -362,7 +362,7 @@ public class OrderDownloadDto {
                 .orderNo(orderNo)
                 .originOrderId(originOrderId)
                 .productId(orderItemInfo == null ? "" : String.valueOf(orderItemInfo.getId()))
-                .skuName((merchandiseLeft == null ? "" : merchandiseLeft) + (orderItemInfo == null ? "" : orderItemInfo.getSkuName()))
+                .skuName((merchandiseLeft == null ? "" : merchandiseLeft) + StrUtil.blankToDefault(order.getSkuNames(), ""))
                 .skuCode(StrUtil.blankToDefault(order.getSkuCodes(), ""))
                 .quantity(String.valueOf(order.getQuantity()))
                 .freebiesName("")
